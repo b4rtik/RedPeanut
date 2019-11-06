5f29b53d6f5c63aee1cb44fc0f490f855088b1bc
b4rtik <andrea.ghelli@gmail.com>
2019-11-06 20:35:44 +0100

    Add SharpMiniDump

---
+++ Managers/ModuleManager/SharpMiniDumpManager.cs
@@ -0,0 +1,92 @@
+﻿//
+// Author: B4rtik (@b4rtik)
+// Project: RedPeanut (https://github.com/b4rtik/RedPeanut)
+// License: BSD 3-Clause
+//
+
+using System;
+using System.Collections.Generic;
+using static RedPeanut.Utility;
+
+namespace RedPeanut
+{
+    class SharpMiniDumpManager : IMenu
+    {
+        public static Dictionary<string, string> mainmenu = new Dictionary<string, string>
+        {
+            { "run", "Execute module" },
+            { "info", "Print help" },
+            { "back", "Back to lateral menu" }
+        };
+
+        public void RePrintCLI()
+        {
+            Utility.RePrintCLI(agent, modulename);
+            return;
+        }
+
+        IAgentInstance agent = null;
+        string modulename = "sharpminidump";
+
+        bool exit = false;
+
+        public SharpMiniDumpManager()
+        {
+
+        }
+
+        public SharpMiniDumpManager(IAgentInstance agent)
+        {
+            this.agent = agent;
+        }
+
+        public void Execute()
+        {
+            exit = false;
+            string input;
+            SetAutoCompletionHandler(mainmenu);
+            do
+            {
+                input = RedPeanutCLI(agent, modulename);
+                MiniDumpMenu(input);
+            } while (!exit);
+        }
+
+        private void MiniDumpMenu(string input)
+        {
+            string f_input = ParseSelection(input);
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                if (mainmenu.ContainsKey(f_input.TrimEnd()))
+                {
+                    switch (f_input.TrimEnd())
+                    {
+                        case "run":
+                            Run();
+                            break;
+                        case "info":
+                            PrintOptions("info", mainmenu);
+                            break;
+                        case "back":
+                            Program.GetMenuStack().Pop();
+                            exit = true;
+                            return;
+                        default:
+                            Console.WriteLine("We had a woodoo");
+                            break;
+                    }
+                }
+                else
+                {
+                    PrintOptions("Command not found", mainmenu);
+                }
+            }
+        }
+
+        private void Run()
+        {
+            RunAssembly(PL_MODULE_SHARPMINIDUMP, "SharpMiniDump.Program", new string[] { }, agent);
+        }
+    }
+}
